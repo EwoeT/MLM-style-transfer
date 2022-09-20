8220f79db8d7edbcb10115c16413fce91a5cf683
Ewoe Tokpo <37966011+EwoeT@users.noreply.github.com>
2022-09-20 13:25:51 +0200

    Add files via upload

---
+++ train_mitigation_model.sh
@@ -0,0 +1,29 @@
+classification_data_path=$1
+bias_train_data_path=$2
+bias_val_data_path=$3
+neutral_train_data_path=$4
+neutral_val_data_path=$5
+
+
+
+python a_bias_class_discriminator.py \
+-data_path $classification_data_path
+
+python b_bias_classification_straight_through.py \
+-train_dataset_path "binary_bias_classification_train_dataset.pt" \
+-val_dataset_path "binary_bias_classification_val_dataset.pt"
+
+python c_latent_embedding_classifier.py \
+-train_dataset_path "binary_bias_classification_train_dataset.pt" \
+-val_dataset_path "binary_bias_classification_val_dataset.pt"
+
+python d_generate_neutral_latent_representation.py \
+-train_dataset_path $bias_train_data_path \
+-val_dataset_path $bias_val_data_path \
+-lambda1 0.5
+
+python e_bias_mitigation_MLM.py \
+-train_dataset_path $neutral_train_data_path \
+-val_dataset_path $neutral_val_data_path \
+-gamma 0.5
+# -test_dataset_path "jigsaw_data/neutral_only.test.en"
